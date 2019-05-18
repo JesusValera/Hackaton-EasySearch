f2bd4a299186c964ffe7131c25e6b61d9894d446
Esther García <esthergb000@gmail.com>
2019-05-18 15:45:24 +0200

    changes in trains view

--- public/js/easysearch.js
+++ public/js/easysearch.js
@@ -1,9 +1,10 @@
 var easysearch = {
-    selectTrain: function(idtrain) {
+    selectTrain: function(idtrain, idsit) {
         //Remove selected class to all train options
         $("[id^='o_idtrain_']").removeClass('selected');
 
         //Select the option clicked
         $('#o_idtrain_' + idtrain).addClass('selected');
+        $('#o_idtrain_' + idtrain + '_sit_' + idsit).addClass('selected');
     }
 }
--- resources/views/train/index.blade.php
+++ resources/views/train/index.blade.php
@@ -22,21 +22,25 @@
             </div>
         </div>
         <?php // foreach train option {?>
-        <div class="row trainoption" id="o_idtrain_1" onclick="easysearch.selectTrain(1);">
+        <div class="row trainoption" id="o_idtrain_1">
             <div class="col-md-12">
                 <div class="row">
                     <div class="col-md-6">15:04 -> 18:05</div>
                     <div class="col-md-6">
                         <div class="row text-center">
-                            <div class="col-md-4">170€</div>
-                            <div class="col-md-4">200€</div>
-                            <div class="col-md-4">210€</div>
+                            <div class="col-md-4 trainoptionsit" id="o_idtrain_1_sit_1" onclick="easysearch.selectTrain(1,1);">170€</div>
+                            <div class="col-md-4 trainoptionsit" id="o_idtrain_1_sit_2" onclick="easysearch.selectTrain(1,2);">200€</div>
+                            <div class="col-md-4 trainoptionsit" id="o_idtrain_1_sit_3" onclick="easysearch.selectTrain(1,3);">210€</div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
         <?php // end foreach train option {?>
+
+        <div class="row">
+
+        </div>
 
     </div>
 @endsection
