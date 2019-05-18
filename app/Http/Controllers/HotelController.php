d7f57ff0595161a6f90c33d9583e8a352d809b28
Jesus Valera <jesus1902@outlook.com>
2019-05-18 13:37:34 +0200

    Train & Hotel controllers created.

---
+++ app/Http/Controllers/HotelController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HotelController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('hotel.index');
+    }
+
+}
