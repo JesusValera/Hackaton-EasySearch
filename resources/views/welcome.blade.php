b6092d4073e8551b0b95d525c5a491d7c37917e9
Esther García <esthergb000@gmail.com>
2019-05-18 18:31:58 +0200

    Main search

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,22 +3,28 @@
 @section('content')
     <div class="container">
         <div class="content">
-            <div class="title m-b-md">
-                EasySearch
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="/img/easysearch.png" alt="logo">
+                </div>
             </div>
-            <div class="links">
-                <div>
-                <h3> Train </h3>
-                    <form class="form-inline" role="form">
-                        <div class="form-group">
+            <div class="row mt-5 searchcontainer">
+                <div class="col-md-12">
+                    <h3> Train </h3>
+                    <form class="" role="form">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <div class="form-group">
                                     <label> FROM </label> <!-- ciudad origen-->
                                     <select id="ciudad_origen" class="form-control">
                                         @foreach($cities as $city)
                                             <option value="{{ $city-> id }}">{{ $city-> nombre }}</option>
                                         @endforeach
                                     </select>
-                        </div>
-                        <div class="form-group" >
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="form-group" >
                                     <label> TO </label>  <!-- ciudad destino-->
                                     <select class="form-control" id="ciudad_destino">
                                         <ul>
@@ -27,35 +33,51 @@
                                             @endforeach
                                         </ul>
                                     </select>
-                        </div>
-                        <div class="form-group" >
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="form-group" >
                                     <label> WHEN </label>
                                     <input class="form-control">
-                                
-                                    <button type="submit" class="btn btn-default"> SEARCH</button>
+                                </div>
+                            </div>
+                            <div class="col-md-3"  style="padding-top:30px;">
+                                <button type="submit" class="btn btn-info"> SEARCH</button>
+                            </div>
                         </div>
                     </form>
-
+                </div>
+            </div>
+            <div class="row mt-5 searchcontainer">
+                <div class="col-md-12">
                     <h3> Hotel </h3>
-                    <form class="form-inline" role="form">
-                        <div class="form-group">
+                    <form class="" role="form">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <div class="form-group">
                                     <label> CITY </label> <!-- ciudad origen-->
                                     <select id="ciudad_origen" class="form-control">
                                         @foreach($cities as $city)
                                             <option value="{{ $city-> id }}">{{ $city-> nombre }}</option>
                                         @endforeach
                                     </select>
-                        </div>
-                        <div class="form-group" >
-                                    <label> FROM </label> 
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="form-group" >
+                                    <label> FROM </label>
                                     <input class="form-control" id="fecha_inicio">
-                                    
-                                    </select>
-                        </div>
-                        <div class="form-group" >
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="form-group" >
                                     <label> TO </label>
                                     <input class="form-control" id="fecha_fin">
-                                    <button type="submit" class="btn btn-default"> SEARCH</button>
+                                </div>
+                            </div>
+                            <div class="col-md-3" style="padding-top:30px;">
+                                <button type="submit" class="btn btn-info"> SEARCH</button>
+                            </div>
                         </div>
                     </form>
                 </div>
