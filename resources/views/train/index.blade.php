6a9102d81000efb0d8ec72dcc14da37088caf5e9
Jesus Valera <jesus1902@outlook.com>
2019-05-18 17:31:21 +0200

    Show trains from db in the view.

--- resources/views/train/index.blade.php
+++ resources/views/train/index.blade.php
@@ -21,22 +21,34 @@
                 </div>
             </div>
         </div>
-        <?php // foreach train option {?>
-        <div class="row trainoption" id="o_idtrain_1">
-            <div class="col-md-12">
-                <div class="row">
-                    <div class="col-md-6 trainoptiondesc">15:04 -> 18:05</div>
-                    <div class="col-md-6">
-                        <div class="row text-center">
-                            <div class="col-md-4 trainoptionsit" id="o_idtrain_1_sit_1" onclick="easysearch.selectTrain(1,1);">170€</div>
-                            <div class="col-md-4 trainoptionsit" id="o_idtrain_1_sit_2" onclick="easysearch.selectTrain(1,2);">200€</div>
-                            <div class="col-md-4 trainoptionsit" id="o_idtrain_1_sit_3" onclick="easysearch.selectTrain(1,3);">210€</div>
+        @foreach($trains as $date => $data)
+
+            <p style="font-size: 2em;">{{ $date }}</p>
+
+            @foreach($data as $train)
+                <div class="row trainoption" id="o_idtrain_1">
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-6 trainoptiondesc">{{ date("h:i", strtotime($train->horasalida)) }}
+                                -> {{ date("h:i", strtotime($train->horallegada)) }}</div>
+                            <div class="col-md-6">
+                                <div class="row text-center">
+                                    <div class="col-md-4 trainoptionsit" id="o_idtrain_1_sit_1"
+                                         onclick="easysearch.selectTrain(1,1);">{{ $train->precio1 }}€
+                                    </div>
+                                    <div class="col-md-4 trainoptionsit" id="o_idtrain_1_sit_2"
+                                         onclick="easysearch.selectTrain(1,2);">{{ $train->precio2 }}€
+                                    </div>
+                                    <div class="col-md-4 trainoptionsit" id="o_idtrain_1_sit_3"
+                                         onclick="easysearch.selectTrain(1,3);">{{ $train->precio3 }}€
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <?php // end foreach train option {?>
+            @endforeach
+        @endforeach
 
         <div class="row">
             <div class="col-md-12 text-right">
